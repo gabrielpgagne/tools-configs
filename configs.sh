b704626ff6bed5fbc2aa38849d629acfca65a44a
Gabriel Gagne <darkoumce.gabgagne@gmail.com>
2023-07-21 15:39:21 -0400

    import mode removal

--- configs.sh
+++ configs.sh
@@ -2,26 +2,11 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ -z "$1" ]
-then # Export mode
-  echo "Backing up configs to repo.
-Make sure the following software is installed:
-  - zsh + oh-my-zsh
-  - kitty terminal
-  - helix IDE"
+echo "Backing up configs to repo.
+- zsh + oh-my-zsh
+- kitty terminal
+- helix IDE"
 
-  cp "${HOME}/.zshrc" "${SCRIPT_DIR}/zsh/.zshrc"
-  cp -r "${HOME}/.config/kitty/kitty.conf" "${SCRIPT_DIR}/kitty/kitty.conf"
-  cp -r "${HOME}/.config/helix/" "${SCRIPT_DIR}/helix"
-
-else # Import mode
-  echo "Importing configs from repo."
-
-  mkdir -p "${HOME}/.config/kitty"
-  mkdir -p "${HOME}/.config/helix"
-
-  cp "${SCRIPT_DIR}/zsh/.zshrc" "${HOME}/.zshrc" 
-  cp -r "${SCRIPT_DIR}/kitty/kitty.conf" "${HOME}/.config/kitty/kitty.conf" 
-  cp -r "${SCRIPT_DIR}/helix" "${HOME}/.config/helix/" 
-
-fi  
+cp "${HOME}/.zshrc" "${SCRIPT_DIR}/zsh/.zshrc"
+cp -r "${HOME}/.config/kitty/" "${SCRIPT_DIR}/kitty/"
+cp -r "${HOME}/.config/helix/" "${SCRIPT_DIR}/helix"
